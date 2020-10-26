45d59fa2d2f16369a961b02889bae23218d07653
Quang Le <iamquang95@gmail.com>
2020-10-26 23:00:55 +0700

    refactor: better ownership

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,7 +3,6 @@ use rsort::{ArraySorter, SortAlgo};
 fn main() {
     let v: Vec<u32> = vec![5, 2, 1, 4, 3];
     let algo = SortAlgo::InserionSort;
-    let mut 
-    solver = ArraySorter::new(v, algo);
+    let mut solver = ArraySorter::new(v.clone(), algo);
     solver.sort();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,38 @@
 pub struct ArraySorter {
     arr: Array,
     algo: SortAlgo,
+    total_swap: u32,
 }
 
 impl ArraySorter {
-
-    pub fn new(
-        arr: Vec<u32>,
-        algo: SortAlgo,
-    ) -> ArraySorter {
+    pub fn new(arr: Vec<u32>, algo: SortAlgo) -> ArraySorter {
         ArraySorter {
             arr: Array::new(arr),
             algo,
+            total_swap: 0,
         }
     }
 
     pub fn sort(&mut self) {
-        self.algo.sort(&mut self.arr)
+        let algo = self.algo.clone();
+        algo.sort(self)
+    }
+
+    fn swap(&mut self, i: usize, j: usize) {
+        std::thread::sleep(std::time::Duration::from_millis(500));
+        self.arr.swap(i, j);
+        self.arr.print();
+        self.total_swap += 1;
     }
 }
 
 struct Array {
-    pub arr: Box<Vec<u32>>
+    pub arr: Box<Vec<u32>>,
 }
 
 impl Array {
-
     fn new(arr: Vec<u32>) -> Array {
-        Array {
-            arr: Box::new(arr)
-        }
+        Array { arr: Box::new(arr) }
     }
 
     fn size(&self) -> usize {
@@ -37,7 +40,6 @@ impl Array {
     }
 
     fn print(&self) {
-        std::thread::sleep(std::time::Duration::from_millis(500));
         println!("{:?}", self.arr);
     }
 
@@ -47,21 +49,20 @@ impl Array {
         self.arr[i] = self.arr[j];
         self.arr[j] = tmp;
     }
-
 }
 
+#[derive(Clone)]
 pub enum SortAlgo {
     InserionSort,
 }
 
 impl SortAlgo {
-    fn sort(&self, arr: &mut Array) {
-        for i in 1..arr.size() {
+    fn sort(&self, arr: &mut ArraySorter) {
+        for i in 1..arr.arr.size() {
             let mut j = i;
-            while j > 0 && arr.arr[j-1] > arr.arr[j] {
-                arr.swap(j, j-1);
+            while j > 0 && arr.arr.arr[j - 1] > arr.arr.arr[j] {
+                arr.swap(j, j - 1);
                 j -= 1;
-                arr.print();
             }
         }
     }
